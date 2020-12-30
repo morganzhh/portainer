8fbc3604c677a85f84abae0dc633c541f3f334d5
alice groux <alice.grx@gmail.com>
2020-12-30 19:55:22 +0100

    refactor(app): backport technical changes (wip)

--- app/kubernetes/helpers/commonHelper.js
+++ app/kubernetes/helpers/commonHelper.js
@@ -12,5 +12,13 @@ class KubernetesCommonHelper {
   static ownerToLabel(owner) {
     return _.replace(owner, /[^-A-Za-z0-9_.]/g, '.');
   }
+
+  static assignOrDeleteIfEmptyOrZero(obj, path, value) {
+    if (!value || value === 0 || (value instanceof Array && !value.length)) {
+      _.unset(obj, path);
+    } else {
+      _.set(obj, path, value);
+    }
+  }
 }
 export default KubernetesCommonHelper;
